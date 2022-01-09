ec3b6f758626abe45c38ddbc4c31631cbba763b6
Hevake <hevake_lcj@126.com>
2022-01-10 07:17:21 +0800

    little optimization

--- tools/build_env.mk
+++ tools/build_env.mk
@@ -3,9 +3,13 @@ export PLATFORM_DIR := $(TOP_DIR)/platforms/$(PLATFORM)
 
 include $(PLATFORM_DIR)/tools/build_env.mk
 
-export CONSTANT_DIR := $(PLATFORM_DIR)/constant_files
-export STAGING_DIR := $(PLATFORM_DIR)/staging_files
-export INSTALL_DIR := $(PLATFORM_DIR)/install_files
+export DESTDIR ?= $(STAGING_DIR)
+export prefix ?= $(STAGING_DIR)
+export TOOLCHAIN_BIN_PREFIX
+
+export CONSTANT_DIR ?= $(PLATFORM_DIR)/constant_files
+export STAGING_DIR ?= $(PLATFORM_DIR)/staging_files
+export INSTALL_DIR ?= $(PLATFORM_DIR)/install_files
 
 export STAGING_INCLUDE := $(STAGING_DIR)/include
 export STAGING_LIB := $(STAGING_DIR)/lib
@@ -18,17 +22,15 @@ LINK_FLAGS := -L$(STAGING_LIB) -L$(CONSTANT_LIB)
 export CFLAGS := $(INCLUDE_FLAGS) -std=c99
 export CXXFLAGS := $(INCLUDE_FLAGS) -std=c++11
 export LDFLAGS := $(LINK_FLAGS)
-export DESTDIR := $(STAGING_DIR)
-export prefix := $(STAGING_DIR)
 
-export AR := $(TOOLCHAIN_PREFIX)ar
-export AS := $(TOOLCHAIN_PREFIX)as
-export CXX := $(TOOLCHAIN_PREFIX)g++
-export CC := $(TOOLCHAIN_PREFIX)gcc
-export NM := $(TOOLCHAIN_PREFIX)nm
-export OBJCOPY := $(TOOLCHAIN_PREFIX)objcopy
-export OBJDUMP := $(TOOLCHAIN_PREFIX)objdump
-export STRINGS := $(TOOLCHAIN_PREFIX)strings
-export SSTRIP := $(TOOLCHAIN_PREFIX)sstrip
-export LSTRIP := $(TOOLCHAIN_PREFIX)lstrip
-export STRIP := $(TOOLCHAIN_PREFIX)strip
+export AR := $(TOOLCHAIN_BIN_PREFIX)ar
+export AS := $(TOOLCHAIN_BIN_PREFIX)as
+export CXX := $(TOOLCHAIN_BIN_PREFIX)g++
+export CC := $(TOOLCHAIN_BIN_PREFIX)gcc
+export NM := $(TOOLCHAIN_BIN_PREFIX)nm
+export OBJCOPY := $(TOOLCHAIN_BIN_PREFIX)objcopy
+export OBJDUMP := $(TOOLCHAIN_BIN_PREFIX)objdump
+export STRINGS := $(TOOLCHAIN_BIN_PREFIX)strings
+export SSTRIP := $(TOOLCHAIN_BIN_PREFIX)sstrip
+export LSTRIP := $(TOOLCHAIN_BIN_PREFIX)lstrip
+export STRIP := $(TOOLCHAIN_BIN_PREFIX)strip
