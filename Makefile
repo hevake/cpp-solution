4fc79706c7009b7d04ebd11d48badbe8684fbeed
Hevake <hevake_lcj@126.com>
2022-01-15 08:29:42 +0800

    Modify tools/configure_helper.sh, solve third_party/libevent compilation problem

--- Makefile
+++ Makefile
@@ -3,7 +3,7 @@ export TOP_DIR := $(PWD)
 include config.mk
 include tools/build_env.mk
 
-#targets += third_party
+targets += third_party
 targets += apps
 
 .PHONY: all test clean distclean $(targets)
@@ -20,6 +20,8 @@ test: $(targets)
 
 clean:
 	rm -rf $(OUTPUT_DIR)
+	make -C third_party clean
 
 distclean: clean
 	rm -rf $(INSTALL_DIR) $(STAGING_DIR)
+	make -C third_party distclean
--- tools/configure_helper.sh
+++ tools/configure_helper.sh
@@ -4,9 +4,9 @@ CCFLAGS="-I$STAGING_INCLUDE -I$CONSTANT_INCLUDE"
 CLDFLAGS="-L$STAGING_LIB -L$CONSTANT_LIB"
 
 ./configure \
-    --libdir=$STAGING_LIB \
-    --includedir=$STAGING_INCLUDE \
-    --bindir=$STAGING_FILES/bin \
+    --libdir=/lib \
+    --includedir=/include \
+    --bindir=/bin \
     CC="${TOOLCHAIN_BIN_PREFIX}gcc" \
     CXX="${TOOLCHAIN_BIN_PREFIX}g++" \
     CFLAGS="$CCFLAGS" \
