ce7fcdcc40c05f3b6668a9f157e4efd63bc10cd7
Hevake <hevake_lcj@126.com>
2022-02-24 07:50:55 +0800

    添加ASan的支持

--- tools/build_env.mk
+++ tools/build_env.mk
@@ -9,22 +9,29 @@ export CONFIGURE_HELPER ?= $(TOP_DIR)/tools/configure_helper.sh
 export MAKE_PATCH ?= $(TOP_DIR)/tools/make_patch.sh
 
 export CONSTANT_DIR ?= $(PLATFORM_DIR)/constant_files
-export STAGING_DIR ?= $(PLATFORM_DIR)/staging_files
-export INSTALL_DIR ?= $(PLATFORM_DIR)/install_files
+export STAGING_DIR  ?= $(PLATFORM_DIR)/staging_files
+export INSTALL_DIR  ?= $(PLATFORM_DIR)/install_files
 
-export STAGING_INCLUDE := $(STAGING_DIR)/include
-export STAGING_LIB := $(STAGING_DIR)/lib
+export STAGING_INCLUDE  := $(STAGING_DIR)/include
+export STAGING_LIB      := $(STAGING_DIR)/lib
 export CONSTANT_INCLUDE := $(CONSTANT_DIR)/include
-export CONSTANT_LIB := $(CONSTANT_DIR)/lib
+export CONSTANT_LIB     := $(CONSTANT_DIR)/lib
 
 export OUTPUT_DIR := $(PLATFORM_DIR)/output_files
 
-INCLUDE_FLAGS := -I$(STAGING_INCLUDE) -I$(CONSTANT_INCLUDE)
-LINK_FLAGS := -L$(STAGING_LIB) -L$(CONSTANT_LIB)
+COMPILE_FLAGS := -I$(STAGING_INCLUDE) -I$(CONSTANT_INCLUDE)
+LINK_FLAGS    := -L$(STAGING_LIB) -L$(CONSTANT_LIB)
 
-export CFLAGS := $(INCLUDE_FLAGS) -std=c99
-export CXXFLAGS := $(INCLUDE_FLAGS) -std=c++11
-export LDFLAGS := $(LINK_FLAGS)
+ifeq ($(RELEASE), 1)
+COMPILE_FLAGS += -O2 -Os
+else
+COMPILE_FLAGS += -fsanitize=address -fno-omit-frame-pointer -DDEBUG=1 -O0 -ggdb
+LINK_FLAGS    += -fsanitize=address -static-libasan
+endif
+
+export CFLAGS   := $(COMPILE_FLAGS) -std=c99
+export CXXFLAGS := $(COMPILE_FLAGS) -std=c++11
+export LDFLAGS  := $(LINK_FLAGS)
 
 export AR := $(TOOLCHAIN_BIN_PREFIX)ar
 export AS := $(TOOLCHAIN_BIN_PREFIX)as
